bc07b44a62b8bbadbaabb13568056de6ffa70e86
venky <venkateswarlu.n@godigit.com>
2023-09-03 19:59:58 +0530

    conflict commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Client1 Master");
+        System.out.println("Hello Client1 Master hello");
     }
 }
